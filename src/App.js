afd3d0ba4763e22133532c03af6015ddb4ae721c
BeingMurshid <murshid717104@gmail.com>
2022-09-21 17:42:18 +0500

    main changes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Head from './componets/Header';
 import Footer from './componets/Footer';
+import Main from './componets/main';
+
 
 function App() {
   return (
     <>
     <Head/>
     <Footer/>
+    <Main/>
     </>
   );
 }
---
+++ src/componets/main.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Main = ()=>{
+    return(
+        <>
+        <div>
+            <h1>Main say : "hello world"</h1>
+        </div>
+        </>
+    )
+}
+export default Main
